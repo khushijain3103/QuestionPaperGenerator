95feb8b951a116a95af61a4693d230e22beeb2f1
khushijain3103 <khushijain312003@gmail.com>
2023-11-25 14:24:07 +0530

    remove node_modules

--- controllers/paper.js
+++ controllers/paper.js
@@ -2,10 +2,10 @@ const Question = require('../models/question');
 
 exports.getQuestionpaper = async (req, res, next) => {
     try {
-      const totalMarks = parseInt(req.query.totalMarks);
-      const easyPercentage = parseFloat(req.query.easyPercentage);
-      const mediumPercentage = parseFloat(req.query.mediumPercentage);
-      const hardPercentage = parseFloat(req.query.hardPercentage);
+      const totalMarks = parseInt(req.query.totalMarks) || 100;
+      const easyPercentage = parseFloat(req.query.easyPercentage) || 30;
+      const mediumPercentage = parseFloat(req.query.mediumPercentage) || 50;
+      const hardPercentage = parseFloat(req.query.hardPercentage) || 20;
 
       const questions = await getPaper(totalMarks, easyPercentage, mediumPercentage, hardPercentage);
 
@@ -19,9 +19,9 @@ async function getPaper(totalMarks, easyPercentage, mediumPercentage, hardPercen
     
 
     // Fetch all questions from the database based on difficulty
-    const allEasyQuestions = await Question.find({ difficulty: 'Easy' }).toArray();
-    const allMediumQuestions = await Question.find({ difficulty: 'Medium' }).toArray();
-    const allHardQuestions = await Question.find({ difficulty: 'Hard' }).toArray();
+    const allEasyQuestions = await Question.find({ difficulty: 'Easy' });
+    const allMediumQuestions = await Question.find({ difficulty: 'Medium' });
+    const allHardQuestions = await Question.find({ difficulty: 'Hard' });
 
     // Function to shuffle an array
     function shuffleArray(array) {
@@ -32,6 +32,8 @@ async function getPaper(totalMarks, easyPercentage, mediumPercentage, hardPercen
         return array;
     }
 
+    // console.log(easyPercentage)
+
     const shuffledEasyQuestions = shuffleArray(allEasyQuestions);
     const shuffledMediumQuestions = shuffleArray(allMediumQuestions);
     const shuffledHardQuestions = shuffleArray(allHardQuestions);
@@ -40,9 +42,15 @@ async function getPaper(totalMarks, easyPercentage, mediumPercentage, hardPercen
     const mediumCount = Math.round((mediumPercentage / 100) * totalMarks);
     const hardCount = Math.round((hardPercentage / 100) * totalMarks);
 
-    const selectedEasyQuestions = selectQuestions(shuffledEasyQuestions, easyCount, totalMarks);
-    const selectedMediumQuestions = selectQuestions(shuffledMediumQuestions, mediumCount, totalMarks);
-    const selectedHardQuestions = selectQuestions(shuffledHardQuestions, hardCount, totalMarks);
+    // console.log(easyCount)
+
+    const selectedEasyQuestions = selectQuestions(0 , 0 , []  , shuffledEasyQuestions, easyCount);
+    const selectedMediumQuestions = selectQuestions(0 , 0 , []  , shuffledMediumQuestions, mediumCount);
+    const selectedHardQuestions = selectQuestions(0 , 0 , []  , shuffledHardQuestions, hardCount);
+
+    console.log('easy' , selectedEasyQuestions);
+    console.log('medium' , selectedMediumQuestions);
+    console.log('hard' , selectedHardQuestions);
 
     const allQuestions = selectedEasyQuestions.concat(selectedMediumQuestions, selectedHardQuestions);
 
@@ -51,7 +59,36 @@ async function getPaper(totalMarks, easyPercentage, mediumPercentage, hardPercen
     return shuffledAllQuestions;
 }
 
-function selectQuestions(){
+function selectQuestions(currentIndex, currentSum, combination , questions , limit){
+
+  // console.log("hey" , currentSum , limit);
+
+  if (currentSum === limit) {
+    console.log('Combination found:', combination);
+
+    return combination;
+  }
+
+  // console.log("questions array" , questions);
+
+  if (currentSum > limit || currentIndex === questions.length) {
+    return;
+  }
+
+  for (let i = currentIndex; i < questions.length; i++) {
+    // console.log(questions[i]);
+    const updatedSum = currentSum + questions[i].marks;
+
+    // console.log(updatedSum);
+
+    const updatedCombination = [...combination, questions[i]];
+
+    let pick = selectQuestions(i + 1, updatedSum, updatedCombination , questions , limit);
+    let notPick = selectQuestions(i + 1, currentSum, combination , questions , limit);
+
+  }
+
+
 
 }
   
